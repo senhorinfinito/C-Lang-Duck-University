3b1ddccb7bc41c37d6359e276d483e8e32640780
Coursera Learner <nobody@nowhere.nul>
2020-08-24 15:10:23 +0000

    Did assignment 0

--- 02_code1/code1.c
+++ 02_code1/code1.c
@@ -1,14 +1,14 @@
-#include <stdio.h>
+0;276;0c#include <stdio.h>
 int max (int num1, int num2) {
   //check if num1 is greater than num2
   if (num1 > num2)
     //if so, your answer is num1
-    printf("%d\n",num1);
+    return num1;
       //otherwise, your answer is num
   else
-    {  printf("%d\n",num2);
+    {  return num2;
     }
-  return 0;
+
 }
 int main(void) {
   printf("max(42, -69) is %d\n", max(42, -69));
