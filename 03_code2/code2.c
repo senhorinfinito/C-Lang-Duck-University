4e071d7c54182042d054bf122fe3686420ec5d82
Coursera Learner <nobody@nowhere.nul>
2020-09-06 11:28:44 +0000

    Did assignment 0

--- 03_code2/code2.c
+++ 03_code2/code2.c
@@ -2,7 +2,8 @@
 #include<stdlib.h>
 int printTriangle(int size) {
   //start with starCount being 0
-  int i,j, starCount;
+  int i,j;
+  float starCount;
     //count from 0 (inclusive) to size (exclusive), for each number i that you count
   for (i=0; i<size; i++){
      //count from 0 (inclusive) to i (inclusive), for each number j that you count
